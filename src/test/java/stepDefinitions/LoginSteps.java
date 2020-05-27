63a68cc019a8b415532f11123b3234a7ab03d10c
Alex Monk <amonk446@gmail.com>
2020-05-27 14:26:15 +1200

    Added a step definition

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -5,13 +5,22 @@ import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Assert;
 
 public class LoginSteps {
 
+    private final WebDriver driver = new FirefoxDriver();
     @Given("I am on the login page")
     public void i_am_on_the_login_page(){
 
+        // check we're on a given web page
+        driver.get("https:\\www.google.com");
     }
     @Given("I enter a {String}")
     public void i_enter_a_username(String username){
