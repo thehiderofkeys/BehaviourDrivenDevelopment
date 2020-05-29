5fa7ee73f3d9ba5813331f8c9bb89b4b0a1c5448
Ryan Lim <ryanlimenwei@gmail.com>
2020-05-29 17:52:34 +1200

    Write Application Code
    * Implemented the basic view for the frontend using ReactJs

---
+++ webapp/front-end/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import LoginPage from "./pages/loginPage";
+import MainPage from "./pages/mainPage";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={LoginPage} />
+          <Route exact path="/main" component={MainPage} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ webapp/front-end/src/pages/mainPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class mainPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    // To something.
+    return (
+      <div>
+        <p>HELLO</p>
+      </div>
+    );
+  }
+}
+
+export default mainPage;
