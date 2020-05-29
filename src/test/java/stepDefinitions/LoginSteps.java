c77af000f2334c69000231368ed1900281e8690f
Alex Monk <amonk446@gmail.com>
2020-05-29 12:06:59 +1200

    Implemented step definitions

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -13,30 +13,43 @@ import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.junit.Assert;
 
+import static org.junit.Assert.assertThat;
+
 public class LoginSteps {
 
     private final WebDriver driver = new FirefoxDriver();
     @Given("I am on the login page")
     public void i_am_on_the_login_page(){
+        // Automation needs to be implemented
+        //driver.get("localhost:3000");
 
         // check we're on a given web page
-        driver.get("https:\\www.google.com");
+        String url = driver.getCurrentUrl();
+        assert(url.equals("localhost:3000"));
     }
     @Given("I enter a {String}")
     public void i_enter_a_username(String username){
+        // Automation needs to be implemented
 
+        WebElement element = driver.findElement(By.id("usernameElement"));
+        String userInput = element.getText();
+        assert(!userInput.equals(""));
     }
-    @Given("I enter a {String}")
-    public void i_enter_a_password(String password){
 
-    }
     @When("I press the login button")
     public void i_press_the_login_button(){
+        // Automation needs to be implemented
 
     }
     @Then("I will be {boolean}")
     public void i_will_be_logged_in(boolean loggedIn){
-        System.out.println("hi there");
+        // Automation needs to be implemented
+
+        // check for the resulting elements that arise from logging in
+        WebElement welcomeMessage = driver.findElement(By.id("welcomeMessage"));
+        assert(welcomeMessage.getText().equals("Welcome"));
+
+        System.out.println("You are now logged in");
     }
 
 
