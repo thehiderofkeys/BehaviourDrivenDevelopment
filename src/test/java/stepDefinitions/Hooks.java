c4287239077322c7af9ff4c8537524271ef81285
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-01 10:36:52 +1200

    Refactor the test code
    Refactor the test code to to work with hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -1,7 +1,7 @@
 package stepDefinitions;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -17,42 +17,37 @@ import javax.annotation.Nullable;
 import static org.junit.Assert.*;
 
 public class LoginSteps {
-    private final WebDriver driver;
-    public LoginSteps(){
-        System.setProperty("webdriver.gecko.driver","../geckodriver.exe");
-        driver = new FirefoxDriver();
-    }
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        driver.get("http://localhost:3000");
+        Hooks.driver.get("http://localhost:3000");
 
         // check we're on a given web page
-        String url = driver.getCurrentUrl();
+        String url = Hooks.driver.getCurrentUrl();
         assert(url.equals("http://localhost:3000"));
     }
     @Given("I enter a username:{string}")
     public void i_enter_a_username(String username){
         // Automation needs to be implemented
-        driver.findElement(By.id("username")).sendKeys(username);
+        Hooks.driver.findElement(By.id("username")).sendKeys(username);
 
-        WebElement element = driver.findElement(By.id("username"));
+        WebElement element = Hooks.driver.findElement(By.id("username"));
         String userInput = element.getText();
         assert(userInput.equals(username));
     }
 
     @Given("I enter a password:{string}")
     public void i_enter_a_password(String password){
-        driver.findElement(By.id("password")).sendKeys(password);
+        Hooks.driver.findElement(By.id("password")).sendKeys(password);
 
-        WebElement element = driver.findElement(By.id("password"));
+        WebElement element = Hooks.driver.findElement(By.id("password"));
         String userInput = element.getText();
         assert(userInput.equals(password));
     }
 
     @When("I press the login button")
     public void i_press_the_login_button(){
-        driver.findElement(By.id("loginButton")).click();
+        Hooks.driver.findElement(By.id("loginButton")).click();
 
         // no test needed
     }
@@ -61,16 +56,12 @@ public class LoginSteps {
         // no automation needed
         // check for the resulting elements that arise from logging in
         try {
-            WebElement welcomeMessage = driver.findElement(By.id("welcomeMessage"));
+            WebElement welcomeMessage = Hooks.driver.findElement(By.id("welcomeMessage"));
             boolean testedLoggedIn = welcomeMessage.getText().equals("Welcome");
             assert (loggedIn.equals(Boolean.toString(testedLoggedIn)));
         }
         catch (NoSuchElementException e){
             assertEquals(loggedIn, "False");
         }
-    }
-    @After
-    public void closeWindow(){
-        driver.close();
     }
 }
