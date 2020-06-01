415e2f9b8351a673b7bccd488ec85b1ca31b25a2
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-01 22:06:54 +1200

    Refactor step definitions

--- src/test/java/stepDefinitions/UnenrollSteps.java
+++ src/test/java/stepDefinitions/UnenrollSteps.java
@@ -13,45 +13,38 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+
 public class UnenrollSteps {
     MainPage mainPage = new MainPage(Hooks.driver);
 
-    @Given("I am on the main page")
-    public void i_am_on_the_main_page(){
-        assert(Hooks.driver.getCurrentUrl().equals("http://localhost:3000/"));
+    @Given("I am logged in as: {string} on the main page to unenroll")
+    public void i_am_logged_in_on_the_main_page(String username) {
+        Hooks.driver.get("http://localhost:3000/"+username+"/main");
+
+        // check we're on a given web page
+        assert(Hooks.driver.getCurrentUrl().equals("http://localhost:3000/"+username+"/main"));
     }
 
-    @Given("I am logged in as: {string}")
-    public void i_am_logged_in(String username){
-        mainPage.logIn(username);
+    @Given("I am enrolled in {string}")
+    public void iAmEnrolledInCurrentEnrolments(String currentEnrolmentsCommaSeparated) {
+        List<String> currentEnrolmentsList = Arrays.asList(currentEnrolmentsCommaSeparated.split(","));
+        assertEquals(currentEnrolmentsList, mainPage.getCurrentEnrolmentsList());
 
-        assert(mainPage.getUsername().equals(username));
     }
 
-    @When("I press the 'unenroll' button next to a listed {string}")
+    @When("I press the 'unenroll' button next to a listed {string} and I click Save")
     public void i_press_the_unenroll_button_next_to_a_listed_course(String course){
         mainPage.pressAnUnenrollButton(course);
+        mainPage.pressSaveButton();
     }
 
-    @Then("I can see my <string> without the <string>, as <string>")
+    @Then("The list of enrolled courses should be updated to {string}")
     public void i_can_see_my_current_enrolments_without_the_specified_course(
-            String currentEnrolmentsCommaSeparated,
-            String specifiedCourse,
             String expectedEnrolmentsCommaSeparated)
     {
         List<String> expectedEnrolments = Arrays.asList(expectedEnrolmentsCommaSeparated.split(","));
 
         assert(mainPage.getCurrentEnrolmentsList().equals(expectedEnrolments));
-    }
-
-    @Then("I can see my <string> as before, and see an <string>")
-    public void i_can_see_my_current_enrolments_and_see_an_error(
-            String currentEnrolmentsCommaSeparated,
-            String errorMessage)
-    {
-        List<String> currentEnrolments = Arrays.asList(currentEnrolmentsCommaSeparated.split(","));
-
-        assert(mainPage.getCurrentEnrolmentsList().equals(currentEnrolments));
-        assert(mainPage.getErrorMessage().equals(errorMessage));
     }
 }
