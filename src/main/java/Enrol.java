e68200731339d9199b7dc2d4da604a8e1f2fa032
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-01 20:53:47 +1200

    Add backend code

--- src/main/java/Enrol.java
+++ src/main/java/Enrol.java
@@ -24,9 +24,13 @@ public class Enrol {
 
 
         // mock user data for pre-enrolled course
-        ArrayList<Course> enrolledCourses = new ArrayList<>();
-        enrolledCourses.add(CourseCatalog.getInstance().search("SOFTENG 754").get(0));
-        enrollmentDatabase.addEnrollment("rightUsrN", enrolledCourses);
+        ArrayList<Course> enrolledCourses1 = new ArrayList<>();
+        enrolledCourses1.add(CourseCatalog.getInstance().search("SOFTENG 754").get(0));
+        ArrayList<Course> enrolledCourses2 = new ArrayList<>();
+        enrolledCourses2.add(CourseCatalog.getInstance().search("SOFTENG 754").get(0));
+        enrolledCourses2.add(CourseCatalog.getInstance().search("SOFTENG 701").get(0));
+        enrollmentDatabase.addEnrollment("rightUsrN", enrolledCourses1);
+        enrollmentDatabase.addEnrollment("user123", enrolledCourses2);
     }
 
     @GET
