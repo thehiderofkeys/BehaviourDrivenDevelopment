99a54328e90655afbac769669a7078ca505613c5
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-01 19:03:43 +1200

    Refactor steps

--- src/test/java/stepDefinitions/ViewCurrentEnrolmentsSteps.java
+++ src/test/java/stepDefinitions/ViewCurrentEnrolmentsSteps.java
@@ -9,6 +9,8 @@ import org.junit.Assert;
 import pageObjects.LoginPage;
 import pageObjects.MainPage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -17,30 +19,11 @@ public class ViewCurrentEnrolmentsSteps {
 
     MainPage mainPage = new MainPage(Hooks.driver);
 
-    @Given("I am on the main page")
-    public void i_am_on_the_main_page(){
-
-
-        // check we're on a given web page
-        assert(Hooks.driver.getCurrentUrl().equals("http://localhost:3000/"));
+    @Given("I am logged in as: {string} on the main page")
+    public void i_am_logged_in_on_the_main_page(String username) {
     }
 
-    @Given("I am logged in as: {String}")
-    public void i_am_logged_in(String username){
-        mainPage.logIn(username);
-
-        assert(mainPage.getUsername().equals(username));
+    @Then("I can see my {string}")
+    public void i_can_see_my_current_enrolments(String courseList) {
     }
-
-    @When ("I press the 'show current enrolments' button")
-    public void i_press_the_show_current_enrolments_button(){
-        mainPage.click_show_current_enrolments_button();
-    }
-
-    @Then ("I can see my {CurrentEnrolments}")
-    public void i_can_see_my_current_enrolments(List<String> currentEnrolmentsList){
-
-        assert(mainPage.getCurrentEnrolmentsList().equals(currentEnrolmentsList));
-    }
-
 }
