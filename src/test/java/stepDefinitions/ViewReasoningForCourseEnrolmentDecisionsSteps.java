d7fe1fad2e5d80c74396e05d45574f364f32db56
Hideaki Murakami <thehiderofkeys@gmail.com>
2020-06-03 14:32:26 +1200

    Refactor step definitions

--- src/test/java/stepDefinitions/ViewReasoningForCourseEnrolmentDecisionsSteps.java
+++ src/test/java/stepDefinitions/ViewReasoningForCourseEnrolmentDecisionsSteps.java
@@ -21,61 +21,44 @@ import static org.junit.Assert.assertEquals;
 
 
 public class ViewReasoningForCourseEnrolmentDecisionsSteps {
+    MainPage mainPage = new MainPage(Hooks.driver);
 
-    public class EnrollSteps {
-        MainPage mainPage = new MainPage(Hooks.driver);
+    // ------------------ EMPTY STEPS FOR READABILITY ------------------
+    // @Given("user123 has completed SOFTENG 401,SOFTENG 402")
+    //      Defined in EnrollSteps.java
 
-        // ------------------ EMPTY STEPS FOR READABILITY ------------------
-        @Given("user123 has completed SOFTENG 401,SOFTENG 402")
-        public void user123_has_completed_SOFTENG_401_and_SOFTENG_402() {}
+    // @Given("rightUsr has completed no courses")
+    //      Defined in EnrollSteps.java
 
-        @Given("rightUsr has completed no courses")
-        public void rightUsr_has_completed_no_courses() {}
+    // @Given("COMPSYS 726 is closed for enrollment")
+    //      Defined in EnrollSteps.java
 
-        @Given("COMPSYS 726 is closed for enrollment")
-        public void SOFTENG_754_is_closed_for_enrollment() {}
+    // ------------------------ SCENARIO STEPS -------------------------
 
-        // ------------------------ SCENARIO STEPS -------------------------
+    //@Given("I am logged in as: {string} on the main page to enroll")
+    //      Defined in EnrollSteps.java
 
-        @Given("I am logged in as: {string} on the main page to enroll")
-        public void i_am_logged_in_on_the_main_page(String username) {
-            Hooks.driver.get("http://localhost:3000/"+username+"/main");
-            // check we're on a given web page
-            assert(Hooks.driver.getCurrentUrl().equals("http://localhost:3000/"+username+"/main"));
-        }
+    //@Given("I enter a {string} into the search bar")
+    //      Defined in EnrollSteps.java
 
-        @Given("I enter a {string} into the search bar")
-        public void i_enter_a_course_name_into_the_search_bar(String courseName){
-            mainPage.enterCourseName(courseName);
+    //Given("I press the 'search' button")
+    //      Defined in EnrollSteps.java
 
-            assertEquals(courseName, mainPage.getSearchBarText());
-        }
+    //@When("I press the 'enroll' button for {string} and press the 'save' button")
+    //      Defined in EnrollSteps.java
 
-        @Given("I press the 'search' button")
-        public void i_press_the_search_button() {
-            mainPage.pressSearchButton();
-        }
+    @When("I click on the expansion button next to the course I failed to enroll in, {string}")
+    public void i_click_on_the_expansion_button_next_to_failed_course(String courseName){
+        
+    }
 
-        @Given("I press the 'enroll' button for {string}")
-        public void i_press_the_enroll_button(String courseName){
-            mainPage.pressAnEnrollButton(courseName);
-        }
-
-        @Given("I press the 'save' button")
-        public void i_press_the_save_button(){
-            mainPage.pressSaveButton();
-        }
-
-        @When("I press the 'update enrolments' button")
-        public void i_press_the_update_enrolments_button(){
-            mainPage.pressUpdateEnrolmentsButton();
-        }
-
-        @Then("A <ErrorMessage> is displayed")
-        public void an_error_message_is_displayed(String errorMessage){
-            assertEquals(mainPage.getErrorMessage(), errorMessage);
-        }
+    @Then("{string} should not be in the list of courses I failed to enroll in")
+    public void course_should_not_be_in_the_list_of_courses_i_failed_to_enroll_in(String courseName){
 
     }
 
+    @Then("A {string} is displayed for the course {string}")
+    public void a_error_message_is_displayed_for_the_course(String errorMessage,String courseName){
+
+    }
 }
